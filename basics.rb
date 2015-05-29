3bb81ef27a7588f0953d230c04123e94ebd64d16
Sean Ward <scward0@gmail.com>
2015-05-29 14:50:05 -0600

    initial sinatra application

---
+++ basics.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  "Hello World!"
+end
+
+get '/about' do
+  "A little about me"
+end
+
+get '/hello/:name' do
+  "Whattup, #{params[:name]}"
+end
+
+get '/hello/:name/:city' do
+  "Whattup #{params[:name]}, your from #{params[:city]}"
+end
+
+get '/more/*' do 
+  params[:splat]
+end
+
+get '/form' do
+  erb :form
+end
+
+post "/form" do
+  "You said, '#{params[:message]}'"
+end
+
+get '/secret' do
+  erb :secret
+end
+
+post '/secret' do
+  params[:secret].reverse
+end
+
+get '/decrypt/:secret' do
+  params[:secret].reverse
+end
+
+not_found do
+  halt 404, 'page not found'
+end
+
